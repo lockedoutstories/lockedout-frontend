9473c46cff7a8458d223a7e672e1e61fae8f7663
Jason O'Neil <jason.oneil@gmail.com>
2020-11-19 00:06:31 +0800

    Attempt to get the function to actually submit a story

--- functions/submit-story.ts
+++ functions/submit-story.ts
@@ -1,5 +1,6 @@
 import { parse } from "querystring";
 import { APIGatewayProxyHandler } from "aws-lambda";
+import wordpress from "wordpress";
 
 export const handler: APIGatewayProxyHandler = (event, context, callback) => {
   let body: any = {};
@@ -20,25 +21,68 @@ export const handler: APIGatewayProxyHandler = (event, context, callback) => {
     });
   }
 
-  // TODO: submit story
+  submitStory(body)
+    .then(() => {
+      // Do redirect for non JS enabled browsers
+      if (
+        event.headers["content-type"] === "application/x-www-form-urlencoded"
+      ) {
+        return callback(null, {
+          statusCode: 302,
+          headers: {
+            Location: "/thanks.html",
+            "Cache-Control": "no-cache",
+          },
+          body: "",
+        });
+      }
 
-  // Do redirect for non JS enabled browsers
-  if (event.headers["content-type"] === "application/x-www-form-urlencoded") {
-    return callback(null, {
-      statusCode: 302,
-      headers: {
-        Location: "/thanks.html",
-        "Cache-Control": "no-cache",
-      },
-      body: "",
+      // Return data to JS Fetch request
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          storySubmitted: true,
+        }),
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: err,
+        }),
+      });
     });
-  }
-
-  // Return data to JS Fetch request
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      storySubmitted: true,
-    }),
-  });
 };
+
+type ExpectedBody = { story: string };
+async function submitStory(body: ExpectedBody) {
+  // Submit a story
+  const client = wordpress.createClient({
+    url: process.env.WORDPRESS_URL,
+    username: process.env.WORDPRESS_USER,
+    password: process.env.WORDPRESS_PASSWORD,
+  });
+
+  const postId = await new Promise((resolve, reject) => {
+    client.newPost(
+      {
+        title: "New draft story submission",
+        status: "draft",
+        content: body.story,
+      },
+      (err, id: string) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(id);
+        }
+      }
+    );
+  });
+
+  console.log(`Created draft post ${postId}`);
+
+  return postId;
+}
